3f4d80aaeaf9a367783ee2ea7678bea20b08e0a4
Chris Guest <chris.guest@trint.com>
2022-05-01 10:19:49 +0100

    Process the SBOMs

--- 01_sqlite/generate.sh
+++ 01_sqlite/generate.sh
@@ -5,7 +5,11 @@ mkdir -p ./sbom
 # TODO: character replacement and add to shell examples
 for image in "ubuntu:20.04" "ubuntu:22.04"; 
 do
-  echo "SBOM for $image"
-  docker sbom --format cyclonedx-json $image > ./sbom/$image.json
+    echo "SBOM for $image"
+    _outfile="${image//:/}"
+    #docker sbom --format cyclonedx-json $image > ./sbom/$_outfile.json
+
+    jq -cr ". | [.metadata.component.name, .metadata.component.type] | @csv" ./sbom/$_outfile.json > ./sbom/$_outfile.csv 
+    jq -cr ".components[] | [.type, .name, .version] | @csv" ./sbom/$_outfile.json > ./sbom/${_outfile}_components.csv 
 done
 
