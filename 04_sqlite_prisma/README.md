2dc8098dd73eb5fa59a6a7931d91bb636d4258a8
Chris Guest <chrisguest75@users.noreply.github.com>
2022-12-29 23:31:35 +0000

    Insert data into schema

--- 04_sqlite_prisma/README.md
+++ 04_sqlite_prisma/README.md
@@ -4,22 +4,38 @@ Demonstrate using prisma with sqlite.
 
 ## Reason
 
+
+## Prerequisites
+
+```sh
+code --install-extension Prisma.prisma
+code --install-extension alexcvzz.vscode-sqlite
+```
+
 ## Start
 
 ```sh
 nvm use
 npm install
 
+npm run start:dev
 
+# seeddb with 10  
+npm run start:dev -- --seed --count 10 
 
+# clean db
+npm run start:dev -- --clean 
 ```
 
 ## Created
 
 ```sh
+npm install @ngneat/falso 
 npm install @prisma/client
 
 npx prisma
+
+npx prisma init --datasource-provider sqlite
 ```
 
 ## Resources
@@ -27,3 +43,10 @@ npx prisma
 * How to Build a Node.js Database using Prisma and SQLite [here](https://www.freecodecamp.org/news/build-nodejs-database-using-prisma-orm/)  
 * NodeJS ESXXXX support [here](https://node.green/)  
 * ES2021 Features with simple examples [here](https://dev.to/carlillo/es2021-features-with-simple-examples-27d3)  
+* falso [here](https://netbasal.com/generate-fake-data-in-the-browser-and-node-js-using-falso-3998d2bcbaaf)
+* falso getting started [here](https://ngneat.github.io/falso/docs/getting-started/)
+* falso examples [here](https://ngneat.github.io/falso/docs/general/#randboolean)
+
+
+https://www.danilucaci.com/blog/reset-and-seed-prisma-database
+https://flaviocopes.com/prisma-clear-database/
--- 04_sqlite_prisma/src/index.ts
+++ 04_sqlite_prisma/src/index.ts
@@ -1,7 +1,92 @@
 import { logger } from './logger'
 import * as dotenv from 'dotenv'
+import * as fs from 'fs'
 import minimist from 'minimist'
+import { PrismaClient } from '@prisma/client'
+import {
+  randNumber,
+  randTextRange,
+  randEmail,
+  randUser,
+} from '@ngneat/falso'
 
+const prisma = new PrismaClient()
+
+/*
+Clean the database
+ */
+async function clean(args: minimist.ParsedArgs) {
+  logger.info(`Cleaning tweets`)
+  await prisma.tweet.deleteMany({})
+  logger.info(`Cleaning users`)
+  await prisma.user.deleteMany({})
+
+  return new Promise((resolve, reject) => {
+    resolve('Complete')
+  })
+}
+
+/*
+Initilize the database
+ */
+async function seed(args: minimist.ParsedArgs) {
+  logger.info(`Initializing DB`)
+
+  const count = Number.parseInt(args['count'])
+  if (count <= 0) {
+    logger.warn('File count is not set')
+  } else {
+    logger.info(`Writing ${count} records`)
+  }
+  for (let i = 0; i < count; i++) {
+    // We create a new user
+    const newUser = await prisma.user.create({
+      data: {
+        email: randEmail(),
+        username: randUser().username,
+      },
+    })
+
+    logger.info(`NewUser:${newUser}`)
+
+    const tweets = randNumber({ min: 1, max: 6 })
+    for (let tweet = 0; tweet < tweets; tweet++) {
+      const firstTweet = await prisma.tweet.create({
+        data: {
+          text: randTextRange({ min: 10, max: 100 }),
+          userId: newUser.id,
+        },
+      })
+
+      logger.info(`Tweet ${tweet}:${firstTweet}`)
+    }
+  }
+
+  return new Promise((resolve, reject) => {
+    resolve('Complete')
+  })
+}
+
+/*
+async function query(args: minimist.ParsedArgs) {
+  return new Promise((resolve, reject) => {
+    logger.info(`Querying DB`)
+    // We fetch the new user again (by its unique email address)
+    // and we ask to fetch its tweets at the same time
+    const newUserWithTweets = await prisma.user.findUnique({
+      where: {
+        email: 'hello@herewecode.io',
+      },
+      include: { tweets: true },
+    })
+  
+    logger.info(`Tweets:${newUserWithTweets}`)
+      resolve('Complete')
+  })
+
+  return true
+}
+*/
 /*
 main
 */
@@ -14,16 +99,21 @@ export async function main(args: minimist.ParsedArgs) {
   logger.error('ERROR - level message')
   logger.fatal('FATAL - level message')
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  if (args['clean']) {
+    await clean(args)
+  }
+
+  if (args['seed']) {
+    await seed(args)
+  }
+
   return new Promise((resolve, reject) => {
-    logger.info('Finishing')
     resolve('Complete')
   })
 }
 
 process.on('exit', async () => {
   logger.warn('exit signal received')
-  process.exit(1)
 })
 
 process.on('uncaughtException', async (error: Error) => {
@@ -35,14 +125,13 @@ process.on('uncaughtException', async (error: Error) => {
 
 process.on('unhandledRejection', async (reason, promise) => {
   logger.error({
-      promise: promise,
-      reason: reason,
-      msg: 'Unhandled Rejection',
+    promise: promise,
+    reason: reason,
+    msg: 'Unhandled Rejection',
   })
   console.log(reason)
   process.exit(1)
 })
-
 
 /*
 Entrypoint
@@ -50,7 +139,12 @@ Entrypoint
 // load config
 dotenv.config()
 logger.info(`Pino:${logger.version}`)
-const args: minimist.ParsedArgs = minimist(process.argv.slice(2))
+const args: minimist.ParsedArgs = minimist(process.argv.slice(2), {
+  string: ['count'],
+  boolean: ['verbose', 'seed', 'clean', 'query'],
+  default: { count: '3', seed: false, clean: false, query: false },
+})
+//const m = promisify(main)
 main(args)
   .then(() => {
     process.exit(0)
@@ -58,4 +152,7 @@ main(args)
   .catch((e) => {
     logger.error(e)
     process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
   })
